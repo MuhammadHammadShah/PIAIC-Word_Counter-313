546faf13a7890948765d6f524921557dbb283019
MuhammadHammadShah <hammad4455shah5544@gmail.com>
2023-05-17 21:01:07 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import figlet from "figlet";
+figlet("Word Counter", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(chalk.green(data));
+});
+async function askQuestion() {
+    const question = await inquirer.prompt([
+        {
+            name: "paragraph",
+            type: "input",
+            message: "Enter your paragraph you want to count the words:",
+        },
+    ]);
+    return question.paragraph;
+}
+async function countWords() {
+    const paragraph = await askQuestion();
+    const arrayOfWords = paragraph.split(" ");
+    const wordCount = arrayOfWords.length;
+    console.log(`Word Count: ${wordCount}`);
+}
+countWords();
